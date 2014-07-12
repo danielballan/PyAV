bc5ffe456345286a64ce33ffe5ce6a2ee8b63f40
danielballan <daniel.b.allan@gmail.com>
2014-07-12 12:46:07 -0500

    WIP: It builds.

--- setup.py
+++ setup.py
@@ -1,146 +1,37 @@
-import ctypes.util
-from setuptools import setup, find_packages, Extension
-import os
-from subprocess import Popen, PIPE
+#!/usr/bin/env/python
 
+from Cython.Distutils import build_ext
+from setuptools import setup, Extension, find_packages
 
-def update_extend(dst, src):
-    for k, v in src.iteritems():
-        dst.setdefault(k, []).extend(v)
+cmdclass = dict(build_ext=build_ext)
 
+av_files = [
+ '_core',
+ 'container',
+ 'format',
+ 'frame',
+ 'logging',
+ 'packet',
+ 'plane',
+ 'stream',
+ 'utils']
 
-def pkg_config(name, macro=False):
-    """Get distutils compatible extension extras via pkg-config."""
+av_video_files = ['format', 'frame', 'plane', 'stream', 'swscontext']
+av_audio_files = ['fifo', 'format', 'frame', 'layout', 'plane', 'resampler', 'stream']
+av_subtitles_files = ['stream', 'subtitle']
 
-    proc = Popen(['pkg-config', '--cflags', '--libs', name], stdout=PIPE, stderr=PIPE)
-    raw_config, err = proc.communicate()
-    if proc.wait():
-        return
+av_extensions = [Extension('av.' + f, sources=['av/{0}.pyx'.format(f)], language='c', include_dirs=['av', '.', 'include'], depends=['av/{0}.pxd'.format(f)]) for f in av_files]
 
-    config = {}
-    if macro:
-        macro_name = name[3:] if name.startswith('lib') else name
-        config['define_macros'] = [('USE_' + macro_name.upper(), '1')]
+av_video_extensions = [Extension('av.video.' + f, sources=['av/video/{0}.pyx'.format(f)], language='c', include_dirs=['av/video', '.', 'include'], depends=['av/video/{0}.pxd'.format(f)]) for f in av_video_files]
 
-    for chunk in raw_config.strip().split():
-        if chunk.startswith('-I'):
-            config.setdefault('include_dirs', []).append(chunk[2:])
-        elif chunk.startswith('-L'):
-            config.setdefault('library_dirs', []).append(chunk[2:])
-        elif chunk.startswith('-l'):
-            config.setdefault('libraries', []).append(chunk[2:])
-        elif chunk.startswith('-D'):
-            name = chunk[2:].split('=')[0]
-            config.setdefault('define_macros', []).append((name, None))
+av_audio_extensions = [Extension('av.audio.' + f, sources=['av/audio/{0}.pyx'.format(f)], language='c', include_dirs=['av/audio', '.', 'include'], depends=['av/audio/{0}.pxd'.format(f)]) for f in av_audio_files]
 
-    return config
-
-
-def check_for_func(lib_names, func_name):
-    """Define macros if we can find the given function in one of the given libraries."""
-
-    if isinstance(lib_names, basestring):
-        lib_names = [lib_names]
-
-    for lib_name in lib_names:
-
-        lib_path = ctypes.util.find_library(lib_name)
-        if not lib_path:
-            print 'Could not find', lib_name, 'with ctypes.util.find_library'
-            continue
-
-        # Open the lib. Look in the path returned by find_library, but also all
-        # the paths returned by pkg-config (since we don't get an absolute path
-        # on linux).
-        lib_paths = [lib_path]
-        lib_paths.extend(
-            os.path.join(root, os.path.basename(lib_path))
-            for root in set(extension_extra.get('library_dirs', []))
-        )
-        for lib_path in lib_paths:
-            try:
-                lib = ctypes.CDLL(lib_path)
-                break
-            except OSError:
-                pass
-        else:
-            print 'Could not find', lib_name, 'with ctypes; looked in:'
-            print '\n'.join('\t' + path for path in lib_paths)
-            continue
-
-        if hasattr(lib, func_name):
-            extension_extra.setdefault('define_macros', []).append(('HAVE_%s' % func_name.upper(), '1'))
-            return
-
-
-
-extension_extra = {
-    'include_dirs': ['include'],
-}
-
-
-# Get the config for the libraries that we require.
-for name in 'libavformat libavcodec libavutil libswscale'.split():
-    config = pkg_config(name)
-    if not config:
-        print 'Could not find', name, 'with pkg-config.'
-    else:
-        update_extend(extension_extra, config)
-
-
-# Get the config for either swresample OR avresample.
-config = pkg_config('libswresample', macro=True)
-if not config:
-    config = pkg_config('libavresample', macro=True)
-if not config:
-    print 'Could not find either of libswresample or libavresample with pkg-config.'
-else:
-    update_extend(extension_extra, config)
-
-
-# Check for some specific functions.
-check_for_func(('avcodec', 'avutil', 'avcodec'), 'av_frame_get_best_effort_timestamp')
-check_for_func('avformat', 'avformat_close_input')
-check_for_func('avformat', 'avformat_alloc_output_context2')
-check_for_func('avutil', 'av_calloc')
-
-
-# Normalize the extras.
-extension_extra = dict((k, sorted(set(v))) for k, v in extension_extra.iteritems())
-
-
-# Construct the modules that we find in the "build/cython" directory.
-ext_modules = []
-build_dir = os.path.abspath(os.path.join(__file__, '..', 'src'))
-for dirname, dirnames, filenames in os.walk(build_dir):
-    for filename in filenames:
-        if filename.startswith('.') or os.path.splitext(filename)[1] != '.c':
-            continue
-
-        path = os.path.join(dirname, filename)
-        name = os.path.splitext(os.path.relpath(path, build_dir))[0].replace('/', '.')
-
-        ext_modules.append(Extension(
-            name,
-            sources=[path],
-            **extension_extra
-        ))
+av_subtitles_extensions = [Extension('av.subtitles.' + f, sources=['av/subtitles/{0}.pyx'.format(f)], language='c', include_dirs=['av/subtitles', '.', 'include'], depends=['av/subtitles/{0}.pxd'.format(f)]) for f in av_subtitles_files]
 
 
 setup(
-
     name='av',
-    version='0.1.0',
-    description='Pythonic bindings for libav.',
-    
-    author="Mike Boers",
-    author_email="pyav@mikeboers.com",
-    
-    url="https://github.com/mikeboers/PyAV",
-
-    packages=find_packages(exclude=['tests', 'examples']),
-    
-    zip_safe=False,
-    ext_modules=ext_modules,
-
+    ext_modules = av_extensions + av_video_extensions + av_audio_extensions + av_subtitles_extensions,
+    packages=find_packages(exclude=['build*', 'tests*', 'examples*']),
+    cmdclass=cmdclass
 )
